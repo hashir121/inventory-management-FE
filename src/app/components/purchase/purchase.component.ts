4d3342b1de1ab05cccf97ad4f49803e8e5364d3b
Hashir-Code <hashirabbasi121@gmail.com>
2025-06-25 10:19:05 +0500

    NA | Installed Anger 19 | 01

--- src/app/components/purchase/purchase.component.ts
+++ src/app/components/purchase/purchase.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-purchase',
-  standalone: true,
-  imports: [],
-  templateUrl: './purchase.component.html',
-  styleUrls: ['./purchase.component.scss']
+    selector: 'app-purchase',
+    imports: [],
+    templateUrl: './purchase.component.html',
+    styleUrls: ['./purchase.component.scss']
 })
 export class PurchaseComponent {
 
--- src/app/components/sales/sales.component.ts
+++ src/app/components/sales/sales.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-sales',
-  standalone: true,
-  imports: [],
-  templateUrl: './sales.component.html',
-  styleUrls: ['./sales.component.scss']
+    selector: 'app-sales',
+    imports: [],
+    templateUrl: './sales.component.html',
+    styleUrls: ['./sales.component.scss']
 })
 export class SalesComponent {
 
